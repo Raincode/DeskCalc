26fdf6c4a5aad11f0a0c7a0edfadfce05e45794c
Matthias Stauber <matthias.stauber@gmx.de>
2017-05-19 21:56:41 +0200

    added Token.hpp

---
+++ Token.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+enum class TType : char {
+	Plus = '+',
+	Minus = '-',
+	Mul = '*',
+	Div = '/',
+	Mod = '%',
+
+	Fac = '!',
+
+	LParen = '(',
+	RParen = ')',
+	LBrace = '{',
+	RBrace = '}',
+	LBracket = '[',
+	RBracket = ']'
+};
+
+struct Token {
+
+};
